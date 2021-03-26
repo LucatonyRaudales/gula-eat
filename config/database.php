3eb9d68765daa687bb91eecc09e147d24637107b
Tony Raudales <traudales.98@icloud.com>
2021-03-26 13:49:31 -0600

    udpate DB password

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'gulaeats_app'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', 'B96C1X0CsimrUDZoaGJR'),
+            'password' => env('DB_PASSWORD', 'jFBcOutPBGBAaXKlRJbc'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
